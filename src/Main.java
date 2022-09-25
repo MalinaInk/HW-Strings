6065bd5b8dfdd0cc5b9739003c0ebf20be664962
mo <nataleshen.ko@yandex.ru>
2022-09-25 19:06:01 +0300

    Задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задание полностью в другой ветке!");
+        //Задание 1
+        String firstName = "Ivanov ";
+        String middleName = "Ivan ";
+        String lastName = "Ivanovich ";
+        String foolName = firstName + middleName + lastName;
+        System.out.println("ФИО сотрудника - " + foolName);
+        //Задание 2
+        String foolName2 = foolName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для отчета - " + foolName2);
+        //Задание 3
+        String foolName3 = "Иванов Семён Семёнович";
+        if (foolName3.contains("ё")) {
+            String newName = foolName3.replace('ё', 'е');
+            System.out.println("Данные сотрудника - " + newName);
+        } else {
+            System.out.println("Данные сотрудника - " + foolName3);
+        }
+
     }
 }
